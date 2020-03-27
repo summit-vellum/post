a2f728107268e7e00b9a2871684d9f4c280714b7
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-03-27 15:01:39 +0800

    set posts fields as nullable

--- src/database/migrations/2019_06_06_115039_create_posts_table.php
+++ src/database/migrations/2019_06_06_115039_create_posts_table.php
@@ -15,15 +15,15 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title', 255);
+            $table->string('title', 255)->nullable();
             $table->text('headline')->nullable();
-            $table->string('slug', 255);
-            $table->longText('content');
-            $table->text('blurb');
+            $table->string('slug', 255)->nullable();
+            $table->longText('content')->nullable();
+            $table->text('blurb')->nullable();
             $table->text('summary')->nullable();
             $table->string('image', 255)->nullable();
             $table->integer('image_id')->nullable()->default(0)->comment('main image id; id is from image library');
-            $table->integer('section_id');
+            $table->integer('section_id')->nullable();
             // 'section_id'
             // 'section'
 
@@ -37,40 +37,40 @@ class CreatePostsTable extends Migration
             $table->integer('video_id')->nullable()->default(0);
             $table->text('video_caption')->nullable();
             $table->integer('gallery_id')->nullable()->default(0);
-            $table->tinyInteger('show_image');
+            $table->tinyInteger('show_image')->nullable();
             $table->string('thumb_image', 255)->nullable();
-            $table->tinyInteger('status')->default(0)->comment('0=Draft, 1=Publish, 2=Disable');
-            $table->string('media_type', 10)->default('Article')->comment('Article, Gallery, Poll, Video');
-            $table->integer('media_id')->default(0);
+            $table->tinyInteger('status')->nullable()->default(0)->comment('0=Draft, 1=Publish, 2=Disable');
+            $table->string('media_type', 10)->nullable()->default('Article')->comment('Article, Gallery, Poll, Video');
+            $table->integer('media_id')->nullable()->default(0);
             $table->float('contributor_fee')->nullable();
             $table->string('editor', 50)->nullable();
-            $table->integer('show_author')->default(1);
-            $table->integer('allow_comments')->default(1);
-            $table->integer('is_published')->default(0)->comment('1=published, 0=never published');
-            $table->integer('is_instant')->default(0);
-            $table->integer('is_news')->default(0);
-            $table->integer('newsroom_id')->default(0);
-            $table->mediumText('syndicated_from');
+            $table->integer('show_author')->nullable()->default(1);
+            $table->integer('allow_comments')->nullable()->default(1);
+            $table->integer('is_published')->nullable()->default(0)->comment('1=published, 0=never published');
+            $table->integer('is_instant')->nullable()->default(0);
+            $table->integer('is_news')->nullable()->default(0);
+            $table->integer('newsroom_id')->nullable()->default(0);
+            $table->mediumText('syndicated_from')->nullable();
             //$table->integer('autosave_id');
-            $table->integer('locked_by')->default(0)->comment('uam user id');
-			$table->integer('lock_timestamp')->default(0);
+            $table->integer('locked_by')->nullable()->default(0)->comment('uam user id');
+			$table->integer('lock_timestamp')->nullable()->default(0);
 
 			$table->tinyInteger('longform')->nullable();
 			$table->tinyInteger('is_nsfw')->nullable();
 			$table->tinyInteger('is_archived')->nullable();
-			$table->integer('fixed')->default(0);
-			$table->integer('is_pushed_notif');
-			$table->integer('is_no_index');
+			$table->integer('fixed')->nullable()->default(0);
+			$table->integer('is_pushed_notif')->nullable();
+			$table->integer('is_no_index')->nullable();
 			$table->string('seo_topic', 255)->nullable();
 			$table->string('seo_keyword', 255)->nullable();
-			$table->integer('total_seo_score')->default(0);
+			$table->integer('total_seo_score')->nullable()->default(0);
 			$table->mediumInteger('search_volume')->nullable();
-			$table->tinyInteger('enable_syndicate')->default(0);
+			$table->tinyInteger('enable_syndicate')->nullable()->default(0);
             $table->integer('syndicate_original_date_published')->nullable();
-            $table->integer('original_date_published')->default(0);
-            $table->integer('date_published')->default(0);
-            $table->integer('date_modified')->default(0);
-            $table->integer('date_created')->default(0);
+            $table->integer('original_date_published')->nullable()->default(0);
+            $table->integer('date_published')->nullable()->default(0);
+            $table->integer('date_modified')->nullable()->default(0);
+            $table->integer('date_created')->nullable()->default(0);
 
             //$table->uuid('uuid')->nullable();
             $table->timestamps();
